ef620f77bf44e044000b85eb46221fc99101f204
velzepooz <velzepooz@gmail.com>
2023-06-26 14:56:13 +0300

    change build script

--- backend/postgrator-config.ts
+++ backend/postgrator-config.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 import { config } from './src/config';
 
-console.log({ config: config.db });
 fs.writeFileSync('.postgratorrc.json', JSON.stringify(config.db, null, 2));
+console.log('File created!!!');
