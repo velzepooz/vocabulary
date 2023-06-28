3a95979ee6d93deac8fb1b0d91008cfe05cd81fe
velzepooz <velzepooz@gmail.com>
2023-06-28 19:24:35 +0300

    add icons

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,9 +9,23 @@ export default defineConfig({
     react(),
     legacy(),
     VitePWA({
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Vocabulary App',
+        short_name: 'Vocabulary',
+        description: 'Vocabulary app',
+        theme_color: '#ffffff',
+        icons: [
+          { src: '/favicon.ico', 'type': 'image/x-icon', sizes: '16x16 32x32' },
+          { src: '/icon-192.png', 'type': 'image/png', sizes: '192x192' },
+          { src: '/icon-512.png', 'type': 'image/png', sizes: '512x512' },
+          { src: '/icon-192-maskable.png', type: 'image/png', sizes: '192x192', purpose: 'maskable' },
+          { src: '/icon-512-maskable.png', type: 'image/png', sizes: '512x512', purpose: 'maskable' }
+        ]
+      },
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: false
       }
     }),
   ],
